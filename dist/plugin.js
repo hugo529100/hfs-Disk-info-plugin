aa2ba09ca6fb0d1ca79f8bf70b4b355e04a86adf
Hug3O <131569471+hugo529100@users.noreply.github.com>
2025-05-15 14:01:25 +0800

    Add files via upload

---
+++ dist/plugin.js
@@ -0,0 +1,5 @@
+exports.description = "Displays disk space info in user panel"
+exports.version = 1
+exports.apiRequired = 1
+exports.frontend_js = 'main.js'
+exports.repo = "Hug3O/Disk-info-plugin"
