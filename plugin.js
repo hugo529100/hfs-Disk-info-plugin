c6e86b28853ed3afe968c0bc62696f0a5e29a0a9
Hug3O <131569471+hugo529100@users.noreply.github.com>
2025-05-15 14:00:53 +0800

    Delete plugin.js

--- plugin.js
+++
@@ -1,5 +0,0 @@
-exports.description = "Displays disk space info in user panel"
-exports.version = 1
-exports.apiRequired = 1
-exports.frontend_js = 'main.js'
-exports.repo = "Hug3O/Disk-info-plugin"
