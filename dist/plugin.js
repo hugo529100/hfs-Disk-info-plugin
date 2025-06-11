99bc9945df9ef44cd524077d96cbade8a5794706
Win_7 <90262580+W-i-n-7@users.noreply.github.com>
2025-06-11 16:51:21 +0300

    fixed incorrect repo

--- dist/plugin.js
+++ dist/plugin.js
@@ -2,4 +2,4 @@ exports.description = "Displays disk space info in user panel"
 exports.version = 1
 exports.apiRequired = 1
 exports.frontend_js = 'main.js'
-exports.repo = "Hug3O/Disk-info-plugin"
+exports.repo = "hugo529100/hfs-disk-info-plugin"
